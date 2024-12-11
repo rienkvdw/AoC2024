80d97fd71c1c15f098fa6979835f958bae8901fa
rienkvdw <rienkvdwijk@gmail.com>
2024-12-11 19:25:52 +0100

    deel 2 nu ook werkend (dubbel dict is vast)

--- ass11.py
+++ ass11.py
@@ -1,51 +1,59 @@
-# assignment 10
+# assignment 11
 import time
+import math
+from collections import defaultdict
+import copy
 
 # uitlezen text bestandje
 with open('input.txt') as inputfile:    # input lezen en splitten in lines
     inputstring = inputfile.read()
     input = inputstring.splitlines()
-    stones = list(map(int,input[0].split()))
+    # stones = list(map(int,input[0].split()))
+    stones = defaultdict(int)
+    for x in input[0].split():
+        stones[x] += 1
 
-def blink(stones, operations):
-    new_stones = []
-    for stone in stones:
-        if stone not in operations:
-            if not stone:
-                operations[stone] = [int(1)]
+def blinkDict(stones, operations):      # nieuwe (snelle?) implementatie met dict
+    new_stones = defaultdict(int)
+    for stonekey, stonevalue in stones.items():
+        if stonekey not in operations:
+            if int(stonekey) == 0:
+                operations[stonekey] = int(1)
             else:
-                digits = len(str(stone))
+                digits = int(math.log10(int(stonekey))+1)   # log10 werkt mooi voor digits vinden
                 if digits%2 == 0:
-                    stone1 = int(stone // 10**(int(digits)/2))
-                    stone2 = int(stone %  10**(int(digits/2)))
-                    operations[stone] = [stone1,stone2]
-                else:
-                    operations[stone] = [stone*2024]
-        new_stones.extend(operations[stone])
+                    stone1 = int(int(stonekey) // 10**(int(digits)/2))
+                    stone2 = int(int(stonekey) %  10**(int(digits/2)))
+                    operations[stonekey] = [stone1,stone2]  # is list (kreeg geen andere goeie manier voor elkaar waarbij ik later niet hoef te checken)
+                else:                                       # keer 2024
+                    operations[stonekey] = int(stonekey)*2024
+        if isinstance(operations[stonekey],list):           # ik doe niet helemaal slim, dus als er twee bij moeten is het edgecase
+            new_stones[operations[stonekey][0]] += stonevalue
+            new_stones[operations[stonekey][1]] += stonevalue
+        else:
+            new_stones[operations[stonekey]] += stonevalue
     return new_stones, operations
 
 # part 1
 ts = time.time()
 blinks1 = 25
 operations = {}
-new_stones,operations = blink(stones,operations)
+new_stones,operations = blinkDict(stones,operations)
 for n in range(blinks1-1):
-    new_stones, operations = blink(new_stones,operations)
-print("Deel 1 kostte " + str(time.time()-ts))       # duurt 0.04s
-new_stones_25 = new_stones.copy()
+    new_stones, operations = blinkDict(new_stones,operations)
+
+print("Deel 1 kostte " + str(time.time()-ts))       # duurt 7.6ms
+new_stones_25 = copy.deepcopy(new_stones)
 
 # part 2
 ts = time.time()
-blinks2 = 40
-# tb = time.time()
+blinks2 = 75
 for n in range(blinks1-1, blinks2-1):
-    # print("Blink number " + str(n) + ", previous blink cost " + str(time.time()-tb))
-    # tb = time.time()
-    new_stones, operations = blink(new_stones,operations)
-print("Deel 2 kostte " + str(time.time()-ts))       # duurt 20s vior blink 40, dus voor 75 gaat het te lang duren
+    new_stones, operations = blinkDict(new_stones,operations)
+print("Deel 2 kostte " + str(time.time()-ts))       # duurt 96.5ms
 
-# printstatements for checking
-# print(stones)
+# # printstatements for checking
+# # print(stones)
 
-print("The total amount of stones after " + str(blinks1) + " blinks is " + str(int(len(new_stones_25))))
-print("The total amount of stones after " + str(blinks2) + " blinks is " + str(int(len(new_stones))))
+print("The total amount of stones after " + str(blinks1) + " blinks is " + str(sum(new_stones_25.values())))
+print("The total amount of stones after " + str(blinks2) + " blinks is " + str(sum(new_stones.values())))
